e26c49e40fe22e8f1cfd458a10ee3fd68cc57d09
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-04-06 00:42:57 +0700

    spacing editing

--- Siput.hpp
+++ Siput.hpp
@@ -9,19 +9,19 @@ class Siput : public BendaAkuarium {
 //Kelas siput ialah turunan dari kelas Benda Akuarium yang merupakan benda yang
 //mengambil koin koin yang ada pada akuarium
 	private :
-	  	const int radiuskoin; //Radius apakah koin dapat diambil atau tidak
-	  	Posisi PointTujuan;
-  public :
-  	//getter 
-  	Posisi getPointTujuan(); //Mengembalikan nilai point tujuan
-  	int getRadiusKoin(); //Mengembalikan nilai Radius Koin dari siput
+		const int radiuskoin; //Radius apakah koin dapat diambil atau tidak
+		Posisi PointTujuan;
+	public :
+		//getter 
+		Posisi getPointTujuan(); //Mengembalikan nilai point tujuan
+		int getRadiusKoin(); //Mengembalikan nilai Radius Koin dari siput
 
-  	//setter
-  	void setPointTujuan(Posisi); //Mengubah nilai Point Tujuan dari Siput
+		//setter
+		void setPointTujuan(Posisi); //Mengubah nilai Point Tujuan dari Siput
 
-  	void gerak(); //Prosedur untuk pergerakan dari iput
-    Posisi cariKoinTerdekat(List<Koin>); //Untuk mencari koin terdekat dari posisi siput
-    void ambilKoin(Koin); //Untuk mengambil suatu koin yang sudan mendekati radius dari siput
+		void gerak(); //Prosedur untuk pergerakan dari iput
+		Posisi cariKoinTerdekat(List<Koin>); //Untuk mencari koin terdekat dari posisi siput
+		void ambilKoin(Koin); //Untuk mengambil suatu koin yang sudan mendekati radius dari siput
 };
 
 #endif
