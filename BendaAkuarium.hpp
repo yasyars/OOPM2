23706c43f70c3ca65ce2fee8c7b309f75d47ac7c
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-04-06 00:35:00 +0700

    Update BendaAkuarium.hpp

--- BendaAkuarium.hpp
+++ BendaAkuarium.hpp
@@ -7,7 +7,7 @@
 class BendaAkuarium : public Movement, public Posisi {
 //Kelas Benda Akuarium ialah benda yang dapat berada pada aquarium
 	public :
-    	virtual void gerak() = 0; //Prosedur ketika sebuah benda aquarium bergerak
+		virtual void gerak() = 0; //Prosedur ketika sebuah benda aquarium bergerak
 };
 
 #endif
