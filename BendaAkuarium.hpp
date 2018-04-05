51d9e690b75424d3bd3308d46518d1595af0c9b9
Tony <buddy90_lost@yahoo.co.id>
2018-04-06 02:42:29 +0700

    wew

--- BendaAkuarium.hpp
+++ BendaAkuarium.hpp
@@ -7,12 +7,9 @@
 class BendaAkuarium : public Movement, public Posisi {
 //Kelas Benda Akuarium ialah benda yang dapat berada pada aquarium
 	public :
-<<<<<<< HEAD
 		BendaAkuarium(int x, int y, int arah, int kec);
     	virtual void gerak() = 0; //Prosedur ketika sebuah benda aquarium bergerak
-=======
 		virtual void gerak() = 0; //Prosedur ketika sebuah benda aquarium bergerak
->>>>>>> e26c49e40fe22e8f1cfd458a10ee3fd68cc57d09
 };
 
 #endif
