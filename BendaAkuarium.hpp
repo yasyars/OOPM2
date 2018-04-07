a19db6884353c2147cd386a048c5db3f3be076e2
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2018-04-08 02:19:11 +0700

    Update BendaAkuarium.hpp

--- BendaAkuarium.hpp
+++ BendaAkuarium.hpp
@@ -7,9 +7,8 @@
 class BendaAkuarium : public Movement, public Posisi {
 //Kelas Benda Akuarium ialah benda yang dapat berada pada aquarium
 	public :
-		BendaAkuarium(int x, int y, int arah, int kec);
-    		virtual void gerak() = 0; //Prosedur ketika sebuah benda aquarium bergerak
-		virtual void gerak() = 0; //Prosedur ketika sebuah benda aquarium bergerak
+		BendaAkuarium(double x, double y, double arah, double kecepatan):Posisi(x, y), Movement(arah, kecepatan){};
+    	virtual void gerak() = 0; //Prosedur ketika sebuah benda aquarium bergerak
 };
 
 #endif
