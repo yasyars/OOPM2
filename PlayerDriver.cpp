0b9b9b5436145aaf73e730bdfff628c5dfa9fa09
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-04-07 13:42:31 +0700

    Add Telur in Atribute and Method

--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -4,12 +4,16 @@
 using namespace std;
 
 int main(){
-	Player Harry(10000);
+	Player Harry(10000, 1);
 	Player Habibi = Harry;
 	Player Tony;
-	Habibi.tambahkoin(200);
-	cout<<"Jumlah Koin Harry: "<<Harry.getJumlahKoin()<<endl;
-	cout<<"Jumlah Koin Habibi: "<<Habibi.getJumlahKoin()<<endl;
-	cout<<"Jumlah Koin Tony: "<<Tony.getJumlahKoin()<<endl;
+	Habibi.tambahKoin(200);
+	Harry.tambahTelur();
+	Tony.tambahTelur();
+	Tony.tambahTelur();
+	Tony.tambahTelur();
+	cout<<"Jumlah Koin Harry: "<<Harry.getJumlahKoin()<<", Jumlah Telur Harry: "<<Harry.getBanyakTelur()<<endl;
+	cout<<"Jumlah Koin Habibi: "<<Habibi.getJumlahKoin()<<", Jumlah Telur Habibi: "<<Habibi.getBanyakTelur()<<endl;
+	cout<<"Jumlah Koin Tony: "<<Tony.getJumlahKoin()<<", Jumlah Telur Tony: "<<Tony.getBanyakTelur()<<endl;
 	return 0;
 }
