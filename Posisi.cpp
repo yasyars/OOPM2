d9146686e01b8758b6f25c622c631e9c3c53a0fb
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-04-07 14:49:46 +0700

    Create Posisi.cpp

---
+++ Posisi.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <cmath>
+#include "Posisi.hpp"
+
+using namespace std;
+
+//Ctor
+//Menginisiasi nilai x dan y dengan 0
+Posisi::Posisi(){
+    x = 0;
+    y = 0;
+}
+//Menginisiasi nilai x dengan parameter int pertama dan nilai y dengan parameter int kedua
+Posisi::Posisi(int a, int b){
+    x = a;
+    y = b;
+}
+//cctor
+Posisi::Posisi(const Posisi& p){
+    x = p.getX(); 
+    y = p.getY();
+}
+//dtor tidak perlu
+//~Posisi();
+
+//operator=
+Posisi& Posisi::operator=(const Posisi& p){
+    x = p.getX();
+    y = p.getY();
+    return *this;
+}
+
+// set
+//Untuk mengubah nilai x dengan sebuah parameter
+void Posisi::setX(int temp){
+    x = temp;
+}
+//Untuk menbubah nilai y dengan integer
+void Posisi::setY(int temp){
+    y = temp;
+}
+
+//getter
+//Untuk mengembalikan nilai x
+int Posisi::getX() const{
+    return x;
+}
+//Untuk mengembalikan nilai y
+int Posisi::getY() const{
+    return y;
+}
+
+//method
+//Untuk menghitung jarak dari 2 posisi
+float hitungjarak(Posisi p1, Posisi p2){
+    return sqrt( pow(p1.getX()-p2.getX(), 2) + pow(p1.getY()-p2.getY(), 2) );
+}
