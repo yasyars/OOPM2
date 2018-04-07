b0a7aa7d7f0041fde77b97c7404c6bd80f5de0c9
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2018-04-08 02:14:01 +0700

    Update Ikan.hpp

--- Ikan.hpp
+++ Ikan.hpp
@@ -6,6 +6,8 @@ using namespace std;
 
 #include "BendaAkuarium.hpp"
 #include "Koin.hpp"
+#include "oop.hpp"
+#include <math.h>
 
 //Kelas ikan adalah kelas yang merupakan derived kelas dari benda akuarium dan merupakan base kelas dari piranha dan guppy
 //Ikan sendiri punya atribut berupa koin yang akan di produksi waktu ikan tahan kenyang
@@ -15,53 +17,71 @@ using namespace std;
 
 class Ikan : public BendaAkuarium {
 	private :
-	  	Koin produksi;
+	  	// Koin produksi;
 	    bool lapar;
-	    const int tahankenyang;
+	    const int tahankenyang; //Waktu untuk berapa lama dia kenyang
 	  	int waktumakan;
-	    const int hunger;
+	    const int hunger; //Waktu untuk menahan lapar
 	    string type;
-		const int radiusmakanan;
+		// const int radiusmakanan; //Waktu 
 		Posisi pointtujuan;
+		string image;
 
 	public :
 	//ctor tanpa parameter dengan kondisi default yang akan didefenisikan kedepannya
-  	Ikan();
+  	Ikan(double x, double y, double arah, double kecepatan);
+
 	//cctor
-    Ikan(const Ikan&);
+    // Ikan(const Ikan&);
+	
 	//dtor
-    ~Ikan();
+    // ~Ikan();
+	
 	//operator overloading untuk assignment
 	Ikan& operator=(const Ikan&);
 	
 	//Mengembalikan koin yang diproduksi oleh ikan
-    Koin getProduksi() const;
+    // Koin getProduksi() const;
+
     //Mengembalikan kondisi ikan lapar menjadi true else false
 	bool getLapar() const;
+
 	//Mengembalikan waktu yang dibutuhkan sampai mencapai kondisi harus memakan/lapar
     int getTahanKenyang() const;
+
 	//Mengembalikan banyak kali ikan makan yang digunakan untuk mengupdate level ikan kedepannya
     int getWaktuMakan() const;
+	
 	//Mengembalikan waktu yang dibutuhkan sampai mati karena kelaparan
 	int getHunger() const;
+	
 	//Mengembalikan nilai point tujuan sebuah ikan
     Posisi getPointTujuan() const;
+	
 	//Mengembalikan type ikan apakah guppy atau piranha
     string getType() const;
     
+    string getImage() const;
+
     //prosedur untuk mengubah produksi koin dari ikan
-    void setProduksi(Koin);
-	//prosedur untuk mengubah kondisi lapar dari ikan
-    void setLapar(bool);
-	//prosedur untuk mengubah waktu makan dari ikan
-    void setWaktuMakan(int);
-    //Prosedur untuk mengubah point tujuan dari ikan
-    void setPointTujuan(Posisi);
+    void setProduksi(Koin x);
 	
+	//prosedur untuk mengubah kondisi lapar dari ikan
+    void setLapar(bool x);
+	
+	//prosedur untuk mengubah waktu makan dari ikan
+    void setWaktuMakan(int x);
+    
+    //Prosedur untuk mengubah point tujuan dari ikan
+    void setPointTujuan(Posisi x);
+	
+    void setImage(string x);
+
   	//virtual method untuk gerak ikan
 	virtual void gerak();
+	
 	//virtual method untuk ikan makan
-	virtual void makan();
+	// virtual void makan();
 	
 };
 
