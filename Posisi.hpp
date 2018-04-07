4707b0f4fa379eb73ce87eb89b42fbdd0e4283b0
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2018-04-08 02:18:37 +0700

    Update Posisi.hpp

--- Posisi.hpp
+++ Posisi.hpp
@@ -3,24 +3,30 @@
 
 class Posisi {
 //Kelas Posisi ialah kelas yang menyimpan posisi suatu benda di akuarium
-    private :
-        int x; //Untuk menyimpan kordinat x dari sebuah benda
-        int y; //Untuk menyimpan kordinat y dari sebuah benda
+  	private :
+      	double x; //Untuk menyimpan kordinat x dari sebuah benda
+        double y; //Untuk menyimpan kordinat y dari sebuah benda
     public :
         //Ctor
       	Posisi(); //Menginisiasi nilai x dan y dengan 0 
-        Posisi(int, int); //Menginisiasi nilai x dengan parameter int pertama dan nilai y dengan parameter int kedua
-    
+        Posisi(double x, double y);
+        //Menginisiasi nilai x dengan parameter int pertama dan nilai y dengan parameter int kedua
+        //cctor
+        Posisi(const Posisi& other);
+        
+        //operator=
+        Posisi& operator=(const Posisi& other);
+
         // sett
-        void setX(int); //Untuk mengubah nilai x dengan sebuah parameter
-        void setY(int); //Untuk menbubah nilai y dengan integer
+        void setX(double x);//Untuk mengubah nilai x dengan sebuah parameter
+        void setY(double y);//Untuk menbubah nilai y dengan integer
         
         //getter
-        int getX() const; //Untuk mengembalikan nilai x
-        int getY() const; //Untuk mengembalikan nilai y
+        double getX() const; //Untuk mengembalikan nilai x
+        double getY() const; //Untuk mengembalikan nilai y
         
         //method
-        friend float hitungjarak(Posisi, Posisi); //Untuk menghitung jarak dari 2 posisi
+        friend double hitungjarak(Posisi y, Posisi x); //Untuk menghitung jarak dari 2 posisi
 };
 
 #endif
