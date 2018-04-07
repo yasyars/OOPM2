9acf3dcf5109a262ce89307269b523585444e0b7
Harry Setiawan Hamjaya <hamjaya_harry@yahoo.co.id>
2018-04-07 17:16:30 +0700

    Update Movement.hpp

--- Movement.hpp
+++ Movement.hpp
@@ -5,27 +5,20 @@ class Movement {
 //Kelas Movement ialah kelas yang mengatur pergerakan dari sebuah benda, yang mempunyai 
 //nilai kecepatan dan arah
 	private :
-  	int arah; //Arah gerak dari sebuah benda yang berisi sudut
-    int kecepatan; //Kecepatan dari sebuah benda
-  public :
-	//Ctor
-    Movement(); //Menginisiasi nilai arah = 0 dan kecepatan = 0
-    Movement(int, int); //Menginisiasi nilai arah = x, dan kecepatan = y
-    //Cctor    
-    Movement(const Movement&); 
-    //Dtor
-    ~Movement();
-    
-    //operator=
-    Movement& operator=(const Movement&);
+  		int arah; //Arah gerak dari sebuah benda yang berisi sudut
+    		int kecepatan; //Kecepatan dari sebuah benda
+  	public :
+		//Ctor
+		Movement(); //Menginisiasi nilai arah = 0 dan kecepatan = 0
+		Movement(int, int); //Menginisiasi nilai arah = x, dan kecepatan = y
 
-    //setter
-    void setArah(int); //Untuk mengubah arah dari benda
-    void setKecepatan(int); //Untuk mengubah kecepatan dari benda
-    
-    //getter
-    int getArah() const; //Untuk mengembalikan nilai arah dari benda
-    int getKecepatan() const;// Untuk mengembalikan nilai kecepatan dari benda
+		//setter
+		void setArah(int); //Untuk mengubah arah dari benda
+		void setKecepatan(int); //Untuk mengubah kecepatan dari benda
+
+		//getter
+		int getArah() const; //Untuk mengembalikan nilai arah dari benda
+		int getKecepatan() const;// Untuk mengembalikan nilai kecepatan dari benda
 };
 
 #endif
