748094ebdbf0ce21f489cfcb851820027e8f7b8b
yasyars <yasya.rusyda@gmail.com>
2018-04-08 16:50:55 +0700

    Menambah ctor dan implementasi gerak

--- Koin.cpp
+++ Koin.cpp
@@ -1,19 +1,41 @@
-#include <iostream>
-#include "Koin.cpp"
-
+#include "Koin.hpp"
 using namespace std;
-
-//Mengembalikan nilai jumlah koin
-static Koin::int getJumlahKoin(){
-	return jumlahkoin;
+Koin::Koin(double x, double y, double kecepatan,int nilai) : BendaAkuarium(x, y, 0, kecepatan) {
+	this->nilai = nilai;
 }
 
+/*
+Koin::Koin(const Koin& koin) {
+
+}
+
+Koin& Koin::operator=(const Koin& koin) {
+
+}
+*/
+
+//Mengembalikan nilai jumlah koi
+/*int Koin::getJumlahKoin(){
+	return jumlahkoin;
+}*/
+
 //Mengembalikan nilai dari sebuah koin
-int Koin::getNilai(){
+int Koin::getNilai() const{
 	return nilai;
+}
+
+//Mengembalikan nama file dari image Koin 
+string Koin::getImage() const {
+	return image;
+}
+
+//Mengubah nama file image koin
+void Koin::setImage(string i) {
+	this->image = i;
 }
 
 //Untuk pergerakan dari sebuah koin
 void Koin::gerak(){
-	// will be done as soon as possible
+	if (abs(this->getY() - SCREEN_HEIGHT) > 0.1)
+		this->setY(this->getY() + this->getKecepatan() * 0.01);
 }
